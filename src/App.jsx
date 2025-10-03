cbf9dbf96dd37c115f3ae233fd948e07b1ba9634
Jason Hernandez <1210444@alumno.um.edu.mx>
2025-10-03 13:47:16 -0600

    upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hola desde mi SPA con React 🚀</h1>
+      <p>Deploy con Vercel y Webpack</p>
+    </div>
+  );
+}
+
+export default App;
